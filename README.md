e1964bcd0a1a94ab3189f207bf5dae5b484f8c8d
SolisAmicus <solisamicus@163.com>
2024-05-12 14:06:05 +0800

    Spring Boot + Spring Data JPA + PostgreSQL example

---
+++ README.md
@@ -0,0 +1,12 @@
+# Spring Boot + Spring data JPA + PostgreSQL example
+
+## Framework
+
+- Spring Boot 3.1.2
+- Spring Data JPA (Hibernate 6 is the default JPA implementation)
+- PostgreSQL 15
+- Java 17
+- Maven 3
+- JUnit 5
+- Spring Tests
+- Docker
---
+++ src/main/java/com/solisamicus/MainApplication.java
@@ -0,0 +1,52 @@
+package com.solisamicus;
+
+import com.solisamicus.model.Book;
+import com.solisamicus.repository.BookRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootApplication
+public class MainApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(MainApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Autowired
+    BookRepository bookRepository;
+
+    // Run this if app.db.init.enabled = true
+    @Bean
+    @ConditionalOnProperty(prefix = "app", name = "db.init.enabled", havingValue = "true")
+    public CommandLineRunner demoCommandLineRunner() {
+        return args -> {
+            System.out.println("Running.....");
+            Book b1 = new Book("Book A",
+                    BigDecimal.valueOf(9.99),
+                    LocalDate.of(2024, 8, 31));
+            Book b2 = new Book("Book B",
+                    BigDecimal.valueOf(19.99),
+                    LocalDate.of(2024, 7, 31));
+            Book b3 = new Book("Book C",
+                    BigDecimal.valueOf(29.99),
+                    LocalDate.of(2024, 6, 10));
+            Book b4 = new Book("Book D",
+                    BigDecimal.valueOf(39.99),
+                    LocalDate.of(2024, 5, 5));
+            bookRepository.saveAll(List.of(b1, b2, b3, b4));
+        };
+    }
+
+}
